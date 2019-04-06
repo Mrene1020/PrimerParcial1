3649d9c6337351ec30e515719921c089683761cf
Your Name <you@example.com>
2019-04-06 09:40:20 -0400

    commit crear un proyecto MVC

---
+++ Friend/Friend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Friend.Startup))]
+namespace Friend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
